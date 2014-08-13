f6d916baad09e3399dc754602cda776e3078e40e
ktupaj <ktupaj@gmail.com>
2014-08-12 23:23:14 -0700

    The first test

---
+++ StarTraderEngineTest/CommodityStorageTest.cs
@@ -0,0 +1,17 @@
+﻿
+namespace StarTrader
+{
+    using Xunit;
+
+    public class CommodityStorageTest
+    {
+        [Fact]
+        public void Can_get_set_size()
+        {
+            var commodity = new CommodityStorage { Size = 5 };
+
+            Assert.Equal(5, commodity.Size);
+            
+        }
+    }
+}
