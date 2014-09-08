261fe13c68dbd2b43a537f54b93402e967bdb115
Pawel Kadluczka <moozzyk@gmail.com>
2014-09-07 21:43:28 -0700

    Enabling creating new game entries and joining/leaving a game

---
+++ StarTrader/Entities/StarTraderContext.cs
@@ -0,0 +1,20 @@
+﻿using System.Data.Entity;
+
+namespace StarTrader.Entities
+{
+    public class StarTraderContext : DbContext
+    {
+        static StarTraderContext()
+        {
+            // TODO: Use migrations once the model gets stabled
+            Database.SetInitializer(new DropCreateDatabaseIfModelChanges<StarTraderContext>());
+        }
+
+        public DbSet<GameEntry> GameEntries { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<GameEntry>().Property(GameEntry.PlayerIdsAsStringExpression);
+        }
+    }
+}
---
+++ StarTraderTest/Models/GameEntryViewModelTest.cs
@@ -0,0 +1,47 @@
+﻿
+using System.Collections.Generic;
+using StarTrader.Entities;
+using Xunit;
+
+namespace StarTrader.Models
+{
+    public class GameEntryViewModelTest
+    {
+        [Fact]
+        public void Can_get_game_entries()
+        {
+            var gameEntries = new List<GameEntry>();
+            var model = new GameEntryViewModel(new Dictionary<string, string>(), gameEntries);
+
+            Assert.Same(gameEntries, model.GameEntries);
+        }
+
+        [Fact]
+        public void Can_get_set_game_name()
+        {
+            var model = new GameEntryViewModel(new Dictionary<string, string>(), new List<GameEntry>())
+            {
+                NewGameName = "new game"
+            };
+
+            Assert.Equal("new game", model.NewGameName);
+        }
+
+        [Fact]
+        public void Can_resolve_exisiting_user()
+        {
+            var model = new GameEntryViewModel(new Dictionary<string, string> {{"07", "Borewicz"}},
+                new List<GameEntry>());
+
+            Assert.Equal("Borewicz", model.GetUserName("07"));
+        }
+
+        [Fact]
+        public void Resolving_non_existing_user_returns_empty_name()
+        {
+            var model = new GameEntryViewModel(new Dictionary<string, string>(), new List<GameEntry>());
+
+            Assert.Equal(string.Empty, model.GetUserName("007"));
+        }
+    }
+}
